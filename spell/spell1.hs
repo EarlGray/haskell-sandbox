8d79f5aed40eb9238b517a1ed56c24bc5eb92849
EarlGray <dmytrish@gmail.com>
2012-08-26 15:56:01 +0300

    Small changes

--- spell/spell1.hs
+++ spell/spell1.hs
@@ -3,15 +3,14 @@ import Control.Monad
 import Data.Char
 
 import qualified Data.ByteString.Lazy.Char8 as L
-import qualified Data.HashTable.IO as H
+import qualified Data.HashTable as H
 
-type HashTable k v = H.CuckooHashTable k v
+type HashTable k v = H.HashTable k v
 
 makedict :: IO (HashTable L.ByteString ())
 makedict = do
   f  <- L.readFile "/usr/share/dict/words"
-  H.fromList (map (flip (,) () . L.map toLower) $ L.lines f)
-  
+  H.fromList H.hashInt $ map ((flip (,) ()) . (L.map toLower)) $ L.lines f
 
 main = do
     [s]  <- getArgs
@@ -21,7 +20,6 @@ main = do
         
 clean' :: L.ByteString -> L.ByteString
 clean' = L.map toLower . L.filter (\x -> isAlpha x || (x == '\'') )
-
 
 spell d w = do res <- H.lookup d (clean' w)
                case res of
