fae7a714232e34c44a7501dff6d3be586b1f0615
EarlGray <dmytrish@gmail.com>
2012-09-22 21:23:06 +0300

    Data.ByteString.UTF8 -> Data.ByteString.Char8
    Haskell:
    real    0m0.314s
    user    0m0.296s
    sys     0m0.015s

--- spell/hspell.hs
+++ spell/hspell.hs
@@ -1,19 +1,15 @@
 import System.Environment (getArgs)
-import qualified Data.ByteString as B
-import qualified Data.ByteString.UTF8 as B
+import qualified Data.ByteString.Char8 as B
 import qualified Data.Set as S
 import Control.Monad 
-
-bwords :: B.ByteString -> [B.ByteString]
-bwords = map B.fromString . words . B.toString
 
 main :: IO ()
 main = do
     dict <- liftM (S.fromList . B.lines) $ B.readFile "/usr/share/dict/words"
     (fname:_) <- getArgs
     file <- B.readFile fname
-    let ws = bwords file
-    mapM_ (putStrLn . B.toString) $ checkWords dict ws
+    let ws = B.words file
+    mapM_ (putStrLn . show) $ checkWords dict ws
 
 checkWords :: S.Set B.ByteString -> [B.ByteString] -> [B.ByteString]
 checkWords d ws = filter (flip S.notMember d) ws
