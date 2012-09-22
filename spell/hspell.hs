e8bb8232c7aed2e1a14c285115f759d66bb7689e
EarlGray <dmytrish@gmail.com>
2012-09-22 22:20:59 +0300

    Data.Set -> Data.Map, no improvement
    Haskell:
    real    0m0.327s
    user    0m0.310s
    sys     0m0.015s

--- spell/hspell.hs
+++ spell/hspell.hs
@@ -1,18 +1,20 @@
 import qualified Data.ByteString.Char8 as B
-import qualified Data.Set as S
+import qualified Data.Map as M
 import Data.Char (isAlpha, toLower)
 import Control.Monad 
 import System.Environment (getArgs)
 
+type MapS = M.Map B.ByteString ()
+
 main :: IO ()
 main = do
-    dict <- liftM (S.fromList . B.lines) $ B.readFile "/usr/share/dict/words"
+    dict <- liftM (M.fromList.map (flip (,)()).B.lines) $ B.readFile "/usr/share/dict/words"
     (fname:_) <- getArgs
     file <- B.readFile fname
     let ws = B.words file
     mapM_ (putStrLn . show) $ checkWords dict ws
 
-checkWords :: S.Set B.ByteString -> [B.ByteString] -> [B.ByteString]
-checkWords d ws = filter (flip S.notMember d . clean) ws
+checkWords :: MapS -> [B.ByteString] -> [B.ByteString]
+checkWords d ws = filter (flip M.notMember d . clean) ws
 
 clean = B.filter (\c -> isAlpha (toLower c) || c `elem` "'")
