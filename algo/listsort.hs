7862130eeee9b9ebd31d0b13f88644de8c6013d7
EarlGray <dmytrish@gmail.com>
2013-03-11 21:06:15 +0200

    testsort

--- algo/listsort.hs
+++ algo/listsort.hs
@@ -17,11 +17,24 @@ mergesort xs = merge (mergesort rxs) (mergesort lxs)
 quicksort [] = []
 quicksort (x:xs) = quicksort (filter (<x) xs) ++ [x] ++ quicksort (filter (>x) xs)
 
+ 
+part :: (a -> Bool) -> [a] -> ([a], [a])
+part p xs = part' xs ([],[])
+ where part' [] (lxs, rxs) = (lxs, rxs)
+       part' (x:xs) (lxs,rxs) | p x = part' xs (x:lxs, rxs)
+       part' (x:xs) (lxs,rxs) = part' xs (lxs, x:rxs)
+
 quicksort' [] = []
 quicksort' (x:xs) = quicksort' lt ++ [x] ++ quicksort' gt
-    where (lt, gt) = partition (<x) xs
+    where (lt, gt) = part (<x) xs
 
-testsort = mergesort
+qs2 [] = []
+qs2 (x:xs) = case part (<x) xs of
+                (lt, gt) -> qs2 lt ++ [x] ++ qs2 gt
+
+rndAlloc n = replicateM n (randomRIO (1::Int, 1000000))
+
+testsort = qs2
 
 main = do
     args <- getArgs
