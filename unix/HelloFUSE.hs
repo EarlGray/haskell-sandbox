16988c7f0087fde659bcb935dfeb97476bc31e8a
EarlGray <dmytrish@gmail.com>
2013-03-20 20:07:28 +0200

    hfuseRead: reading the hardcoded file

--- unix/HelloFUSE.hs
+++ unix/HelloFUSE.hs
@@ -5,6 +5,7 @@ module Main where
  -
  -}
 
+import Control.Arrow (second)
 import Control.Exception
 import Data.Maybe
 import Data.List
@@ -15,7 +16,11 @@ import System.Posix.Types
 import System.Posix.Time
 import Numeric (showOct)
 
-helloFUSE :: FuseOperations Handle
+import qualified Data.ByteString.UTF8 as B
+
+data HT = HT ()
+
+helloFUSE :: FuseOperations HT
 helloFUSE = defaultFuseOps {
     fuseInit = hfuseInit,
     fuseDestroy = hfuseDestroy,
@@ -25,24 +30,31 @@ helloFUSE = defaultFuseOps {
     fuseGetFileStat = hfuseGetAttr,
 
     fuseOpenDirectory = hfuseOpenDir,
-    fuseReadDirectory = hfuseReadDir
+    fuseReadDirectory = hfuseReadDir,
+
+    fuseOpen = hfuseOpen,
+    fuseRead = hfuseRead
 }
 
 fsstat :: FileSystemStats
 fsstat = FileSystemStats {
-      fsStatBlockSize = 512,
-      fsStatBlockCount = 1,
-      fsStatBlocksFree = 0,
-      fsStatBlocksAvailable = 0,
-      fsStatFileCount = 20,
-      fsStatFilesFree = 0,
-      fsStatMaxNameLength = 255
+    fsStatBlockSize = 512,
+    fsStatBlockCount = 1,
+    fsStatBlocksFree = 0,
+    fsStatBlocksAvailable = 0,
+    fsStatFileCount = 20,
+    fsStatFilesFree = 0,
+    fsStatMaxNameLength = 255
 }
 
+greetings = B.fromString "Greetings, we come in piece!"
+hello = "/hello"
 fileattrs = [
     --(".",       FileStat Directory   (CMode 0o755) 1 (CUid 0) (CGid 0) (CDev 0) 512 1 0 0 0),
     ("/",       FileStat Directory   (CMode 0o755) 1 (CUid 0) (CGid 0) (CDev 0) 512 1 0 0 0),
-    ("/hello",  FileStat RegularFile (CMode 0o644) 1 (CUid 0) (CGid 0) (CDev 0) 512 1  0 0 0)]
+    (hello,     FileStat RegularFile (CMode 0o644) 1
+                         (CUid 0) (CGid 0) (CDev 0)
+                         (fromIntegral $ B.length greetings) 1  0 0 0)]
 
 
 hfuseInit = putStrLn "hfuseInit" >> hFlush stdout
@@ -81,10 +93,25 @@ hfuseReadDir dirpath = do
     time <- epochTime
     let mkFName (fname,info) | fname == dirpath = Just (".",info)
                              | otherwise = (flip (,) info) `fmap` stripPrefix dirpath fname
-        fattrs = mapMaybe mkFName fileattrs
+        stampData fs = fs { statFileOwner = uid, statFileGroup = gid,
+                            statAccessTime = time,  statModificationTime = time, statStatusChangeTime = time }
+        fattrs = mapMaybe (mkFName . (second stampData)) fileattrs
     case dirpath of
       "/" -> return $ Right fattrs
       _ -> return $ Left eNOENT
+
+hfuseOpen :: FilePath -> OpenMode -> OpenFileFlags -> IO (Either Errno HT)
+hfuseOpen path mode _ | path == hello =
+    case mode of
+      ReadOnly -> return $ Right $ HT ()
+      _ -> return $ Left eACCES
+hfuseOpen _ _ _ = return $ Left eNOENT
+
+hfuseRead :: FilePath -> HT -> ByteCount -> FileOffset -> IO (Either Errno B.ByteString)
+hfuseRead path _ byteCount offset
+    | path == hello =
+        return $ Right $ B.take (fromIntegral byteCount) $ B.drop (fromIntegral offset) greetings
+    | otherwise = return $ Left eNOENT
 
 handleExeption :: SomeException -> IO Errno
 handleExeption e = do
