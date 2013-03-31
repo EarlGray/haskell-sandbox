c50c4121f939c9b21a35b0a09cc57465cc4fe8b5
EarlGray <dmytrish@gmail.com>
2013-03-31 14:26:08 +0300

    Command-line options
    Now Main is a functional utility which allows to choose sorting
    method, number of items, switch between arrays and lists and so on:
    
    $ ghc --make Main.hs -o hsort
    $ ./hsort --help

--- algo/sort/Main.hs
+++ algo/sort/Main.hs
@@ -1,29 +1,72 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
-import SortingCommon
+import SortingCommon hiding (array)
 import QuickSort
 import BubbleSort
+import SelectSort
 import MergeSort
 
-import System.Random
+import Data.Maybe (fromMaybe)
+import Control.Applicative ((<$>), (<*>))
 import Control.Monad (replicateM)
+
+import System.Random
+import System.Console.CmdArgs
+
+data CommandOptions = CommandOptions {
+        sort :: String,
+        size :: Int,
+        array, rand :: Bool
+    } deriving (Show, Data, Typeable)
+defaultOpts = CommandOptions "bubble" 25 False False
+
+mkArray xs = listArray (0, length xs - 1) xs
 
 randomList :: Int -> IO [Int]
 randomList n = replicateM n (randomRIO(1,1000000))
 
 randomArray :: Int -> IO (UArray Int Int)
 randomArray n = mkArray `fmap` randomList n
-  where mkArray xs = listArray (0, length xs - 1) xs
 
+sorts :: Ord e => [(String, [e] -> [e])]
+sortAs :: [(String, UArray Int Int -> UArray Int Int)]
+--sortAs :: (Ord e, Ix i) => M.Map String (UArray i e -> UArray i e)
+
+sorts = [
+    ("bubble", bubblesort),
+    ("merge", mergesort),
+    ("quick", quicksort),
+    ("select", selectsort) ]
+
+sortAs = [
+    ("bubble", bubblesortA) ]
+
+readLst :: String -> [Int]
+readLst = read
 
 main :: IO ()
 main = do
-    putStrLn "List bubblesort:"
-    list <- randomList 25
-    print list
-    print $ bubblesort list
+    opts <- cmdArgs defaultOpts
+    let n = size opts
 
-    putStrLn "Array bubblesort:"
-    arr <- randomArray 25
-    print (elems arr)
-    print $ elems $ bubblesortA arr
+    if array opts 
+    then do
+        let sortf = fromMaybe bubblesortA (lookup (sort opts) sortAs)
+        arr <- if rand opts 
+                then do
+                      a <- randomArray n
+                      print $ elems a
+                      return a
+                else mkArray <$> readLst <$> getContents
+        print $ elems $ sortf arr
+    else do
+        let sortf = fromMaybe bubblesort (lookup (sort opts) sorts)
+        list <- if rand opts
+                then do
+                        list <- randomList n
+                        print list
+                        return list
+                else readLst <$> getContents
+        print $ sortf list
+
