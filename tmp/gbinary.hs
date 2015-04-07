a092cf9e83313ace7cb830a2a1554653ac7fdbc5
Dmytro S <dmytrish@gmail.com>
2015-04-07 20:33:52 +0300

    omit: working `omit cat-file` and `omit log`

--- tmp/gbinary.hs
+++ tmp/gbinary.hs
@@ -1,18 +1,22 @@
-{-# LANGUAGE DeriveGeneric #-}
-import Data.Binary
-import Data.Binary.Get
+{-# LANGUAGE DeriveDataTypeable #-}
+
 import Data.Bits
 
-import Data.Generics
+import Data.Binary
+import Data.Binary.Get
+import Data.Binary.Generic
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+
+import Data.Data
 
 data BMPHeader = BMPHeader {
     bmphMagic :: Word16,
     bmphFileSize :: Word32,
     bmphReserved :: Word32,
     bmphOffset :: Word32
-} deriving (Generic, Show)
-
-instance Binary BMPHeader
+} deriving (Data, Typeable, Show)
 
 main = do
-    print $ 
+    bytes <- BL.readFile "tru256.bmp"
+    print $ runGet (getGeneric :: Get BMPHeader) bytes
