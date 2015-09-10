e93176e5410fd749a6772c3263192c666764572e
Dmytro S <dmytrish@gmail.com>
2015-09-10 14:12:50 +0300

    proper State.hs

--- lang/fp/state.hs
+++ lang/fp/state.hs
@@ -1,3 +1,10 @@
+module State (
+  State(..),
+  get, put, modify,
+  evalState, execState
+) where
+import Control.Applicative
+
 newtype State s a = State {
   runState :: s -> (a, s)
 }
@@ -8,12 +15,28 @@ get = State (\s -> (s, s))
 put :: s -> State s ()
 put x = State (\_ -> ((), x))
 
+modify :: (s -> s) -> State s ()
+modify f = State (\s -> ((), f s))
+
+unit :: a -> State s a
+unit a = State (\s -> (a, s))
+
+bind :: State s a -> (a -> State s b) -> State s b
+bind st f = State (\s -> let (a, s') = runState st s in runState (f a) s')
+
 instance Functor (State s) where
   fmap f t = State (\s -> let (a, s') = runState t s in (f a, s'))
 
+instance Applicative (State s) where
+  pure    = unit
+  f <*> a = State (\s -> let (va, s') = runState a s in let (vf, s'') = runState f s' in (vf va, s''))
+
 instance Monad (State s) where
-  return x = State (\s -> (x, s))
-  x >>= f  = State (\s -> let (a, s') = runState x s in runState (f a) s')
+  return  = unit
+  (>>=)   = bind
 
 evalState :: State s a -> s -> a
-evalState t s = fst $ runState t s
+evalState t = fst . runState t
+
+execState :: State s a -> s -> s
+execState t = snd . runState t
